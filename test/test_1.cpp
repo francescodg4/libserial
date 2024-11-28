dddfa3628c6b472d9250a379bf3e7d11d3a91706
Francesco de Gioia <francesco.degioia@alkeria.com>
2024-11-28 14:56:09 +0100

    added test with catch2

---
+++ test/test_1.cpp
@@ -0,0 +1,17 @@
+#include <chrono>
+
+#include "catch2/catch_all.hpp"
+#include "serial/serial.h"
+
+TEST_CASE("serial::Timeout::simpleTimeout creates a serial::Timeout with appropriate initialization", "[serial]")
+{
+    auto timeout = std::chrono::milliseconds(500);
+
+    serial::Timeout serial_timeout = serial::Timeout::simpleTimeout(static_cast<uint32_t>(timeout.count()));
+
+    REQUIRE(serial_timeout.inter_byte_timeout == serial::Timeout::max());
+    REQUIRE(serial_timeout.read_timeout_constant == static_cast<uint32_t>(timeout.count()));
+    REQUIRE(serial_timeout.read_timeout_multiplier == 0);
+    REQUIRE(serial_timeout.write_timeout_constant == static_cast<uint32_t>(timeout.count()));
+    REQUIRE(serial_timeout.write_timeout_multiplier == 0);
+}
